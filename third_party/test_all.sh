1ef1dfd9ce77232e2f921b1bb59e21d3f2532a3b
Yu Ding <dingyu02@baidu.com>
2018-01-22 19:56:15 -0800

    fix test script

--- third_party/test_all.sh
+++ third_party/test_all.sh
@@ -32,7 +32,9 @@ test_cases=`ls`
 
 for i in ${test_cases[@]}
 do
-	cd ${i} && xargo build --target x86_64-unknown-linux-sgx --release && git clean -fxd && cd ..
+	if [ "${i}" != 'serde-rs' ]; then
+		cd ${i} && xargo build --target x86_64-unknown-linux-sgx --release && git clean -fxd && cd ..
+	fi
 done
 
 echo "Done!"
