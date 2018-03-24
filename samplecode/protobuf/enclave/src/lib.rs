7e4bd7afc99a50af6613d9d5d237260933334929
Yu Ding <dingyu02@baidu.com>
2018-03-24 00:33:22 -0700

    v0.9.8 release

---
+++ samplecode/protobuf/enclave/src/lib.rs
@@ -0,0 +1,63 @@
+// Copyright (C) 2017-2018 Baidu, Inc. All Rights Reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materials provided with the
+//    distribution.
+//  * Neither the name of Baidu, Inc., nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#![crate_name = "helloworldsampleenclave"]
+#![crate_type = "staticlib"]
+
+#![cfg_attr(not(target_env = "sgx"), no_std)]
+#![cfg_attr(target_env = "sgx", feature(rustc_private))]
+
+extern crate sgx_types;
+#[cfg(not(target_env = "sgx"))]
+#[macro_use]
+extern crate sgx_tstd as std;
+
+use sgx_types::*;
+use std::string::String;
+use std::io::{self, Write};
+use std::slice;
+
+extern crate protobuf;
+mod person;
+use person::Person;
+use protobuf::hex::decode_hex;
+use protobuf::parse_from_bytes;
+
+#[no_mangle]
+pub extern "C" fn say_something(some_string: *const u8, some_len: usize) -> sgx_status_t {
+
+    let str_slice = unsafe { slice::from_raw_parts(some_string, some_len) };
+    let _ = io::stdout().write(str_slice);
+
+    let decoded = decode_hex(&String::from_utf8(str_slice.to_vec()).unwrap());    
+    let parsed = parse_from_bytes::<Person>(&decoded).unwrap();
+    
+    println!("\nname: {}, id: 0x{:08X}, email at: {}", parsed.get_name(), parsed.get_id(), parsed.get_email() );
+
+    sgx_status_t::SGX_SUCCESS
+}
---
+++ third_party/protobuf/src/lib.rs
@@ -0,0 +1,115 @@
+#![crate_type = "lib"]
+#![no_std]
+#![feature(slice_concat_ext)]
+
+#![cfg_attr(target_env = "sgx", feature(rustc_private))]
+
+#[cfg(feature = "bytes")]
+extern crate bytes;
+
+#[cfg(not(target_env = "sgx"))]
+#[macro_use]
+extern crate sgx_tstd as std;
+
+#[cfg(target_env = "sgx")]
+#[macro_use]
+extern crate std;
+
+pub use unknown::UnknownFields;
+pub use unknown::UnknownFieldsIter;
+pub use unknown::UnknownValue;
+pub use unknown::UnknownValueRef;
+pub use unknown::UnknownValues;
+pub use unknown::UnknownValuesIter;
+pub use repeated::RepeatedField;
+pub use singular::SingularField;
+pub use singular::SingularPtrField;
+pub use clear::Clear;
+pub use protocore::Message;
+pub use protocore::MessageStatic;
+pub use protocore::ProtobufEnum;
+pub use protocore::parse_from_bytes;
+pub use protocore::parse_from_reader;
+#[cfg(feature = "bytes")]
+pub use protocore::parse_from_carllerche_bytes;
+pub use protocore::parse_length_delimited_from;
+pub use protocore::parse_length_delimited_from_bytes;
+pub use stream::CodedInputStream;
+pub use stream::CodedOutputStream;
+pub use stream::wire_format;
+pub use error::ProtobufResult;
+pub use error::ProtobufError;
+pub use cached_size::CachedSize;
+#[cfg(feature = "bytes")]
+pub use chars::Chars;
+
+// generated
+pub mod descriptor;
+pub mod plugin;
+mod rustproto;
+
+pub mod protocore;
+pub mod rt;
+pub mod lazy;
+//pub mod code_writer;
+//pub mod codegen;
+pub mod compiler_plugin;
+pub mod repeated;
+pub mod singular;
+pub mod clear;
+pub mod reflect;
+pub mod text_format;
+pub mod stream;
+pub mod error;
+pub mod types;
+pub mod well_known_types;
+pub mod ext;
+
+// used by test
+pub mod hex;
+
+// used by rust-grpc
+pub mod descriptorx;
+
+mod zigzag;
+mod paginate;
+mod unknown;
+mod strx;
+mod rust;
+mod cached_size;
+mod varint;
+#[cfg(feature = "bytes")]
+pub mod chars; // TODO: make private
+
+mod misc;
+
+mod buf_read_iter;
+
+
+// so `use protobuf::*` could work in mod descriptor and well_known_types
+mod protobuf {
+    pub use descriptor;
+    pub use descriptorx;
+//    pub use codegen;
+    pub use reflect;
+    pub use protocore::*;
+    pub use error::*;
+    pub use stream::*;
+    pub use rt;
+    pub use text_format;
+    pub use types;
+    pub use lazy;
+    pub use well_known_types;
+    pub use ext;
+    pub use unknown::UnknownFields;
+    pub use unknown::UnknownFieldsIter;
+    pub use unknown::UnknownValue;
+    pub use unknown::UnknownValueRef;
+    pub use unknown::UnknownValues;
+    pub use unknown::UnknownValuesIter;
+    pub use repeated::RepeatedField;
+    pub use singular::SingularField;
+    pub use singular::SingularPtrField;
+    pub use clear::Clear;
+    pub use cached_size::CachedSize;
+}
