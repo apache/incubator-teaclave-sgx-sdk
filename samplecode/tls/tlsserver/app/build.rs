f1bd1e0f130f2d3822046072901c15a02aac60f8
Yu Ding <dingyu02@baidu.com>
2018-05-23 15:17:24 -0700

    Support simulation mode in more sample codes

--- samplecode/tls/tlsserver/app/build.rs
+++ samplecode/tls/tlsserver/app/build.rs
@@ -32,10 +32,16 @@ fn main () {
 
     let sdk_dir = env::var("SGX_SDK")
                     .unwrap_or_else(|_| "/opt/intel/sgxsdk".to_string());
+    let is_sim = env::var("SGX_MODE")
+                    .unwrap_or_else(|_| "HW".to_string());
 
     println!("cargo:rustc-link-search=native=../lib");
     println!("cargo:rustc-link-lib=static=Enclave_u");
 
     println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
-    println!("cargo:rustc-link-lib=dylib=sgx_urts");
+    match is_sim.as_ref() {
+        "SW" => println!("cargo:rustc-link-lib=dylib=sgx_urts_sim"),
+        "HW" => println!("cargo:rustc-link-lib=dylib=sgx_urts"),
+        _    => println!("cargo:rustc-link-lib=dylib=sgx_urts"), // Treat undefined as HW
+    }
 }
