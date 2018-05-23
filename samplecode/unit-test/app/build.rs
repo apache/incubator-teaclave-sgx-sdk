8598882d47411403070c7908ad8e58149b8076dc
Yu Ding <dingyu02@baidu.com>
2018-05-23 15:15:14 -0700

    Support simulation mode in more sample codes

--- samplecode/unit-test/app/build.rs
+++ samplecode/unit-test/app/build.rs
@@ -31,12 +31,22 @@ use std::env;
 fn main () {
     let sdk_dir = env::var("SGX_SDK")
                     .unwrap_or_else(|_| "/opt/intel/sgxsdk".to_string());
+    let is_sim = env::var("SGX_MODE")
+                    .unwrap_or_else(|_| "HW".to_string());
 
     println!("cargo:rustc-link-search=native=../lib");
     println!("cargo:rustc-link-lib=static=Enclave_u");
 
     println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
-    println!("cargo:rustc-link-lib=dylib=sgx_urts");
-    println!("cargo:rustc-link-lib=dylib=sgx_uae_service");
     println!("cargo:rustc-link-lib=static=sgx_uprotected_fs");
+    match is_sim.as_ref() {
+        "SW" => {
+            println!("cargo:rustc-link-lib=dylib=sgx_urts_sim");
+            println!("cargo:rustc-link-lib=dylib=sgx_uae_service_sim");
+        },
+        _    => { // HW by default
+            println!("cargo:rustc-link-lib=dylib=sgx_urts");
+            println!("cargo:rustc-link-lib=dylib=sgx_uae_service");
+        },
+    }
 }
