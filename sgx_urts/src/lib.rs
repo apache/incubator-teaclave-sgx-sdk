cf10764633b643c336e333289acb791b77b21d85
Yu Ding <dingelish@gmail.com>
2019-05-07 16:31:35 -0700

    Update copyright header to 2019

--- sgx_urts/src/lib.rs
+++ sgx_urts/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2017-2018 Baidu, Inc. All Rights Reserved.
+// Copyright (C) 2017-2019 Baidu, Inc. All Rights Reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
