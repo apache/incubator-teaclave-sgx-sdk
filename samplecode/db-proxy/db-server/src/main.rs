615d2248cfbe21d3ddd9c16d41a9c41e1b6e8a2a
webb.shi <bradyjoestar@gmail.com>
2019-05-21 03:37:01 -0400

    add rocksdb base in untrusted db-server

--- samplecode/db-proxy/db-server/src/main.rs
+++ samplecode/db-proxy/db-server/src/main.rs
@@ -1,3 +1,26 @@
+extern crate libc;
+extern crate rocksdb;
+
+mod util;
+
+use libc::size_t;
+
+use rocksdb::{DBVector, Error, IteratorMode, Options, WriteBatch, DB};
+use util::DBPath;
+
 fn main() {
-    println!("Hello, world!");
+    let path = DBPath::new("_rust_rocksdb_snapshottest");
+    {
+        let db = DB::open_default(&path).unwrap();
+
+        assert!(db.put(b"k1", b"v1111").is_ok());
+
+        let snap = db.snapshot();
+        assert!(snap.get(b"k1").unwrap().unwrap().to_utf8().unwrap() == "v1111");
+
+        assert!(db.put(b"k2", b"v2222").is_ok());
+
+        assert!(db.get(b"k2").unwrap().is_some());
+        assert!(snap.get(b"k2").unwrap().is_none());
+    }
 }
