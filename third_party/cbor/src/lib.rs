069d0f1b7a8005d5c71a5817a438df612cb9946d
Yu Ding <dingelish@gmail.com>
2019-05-25 23:12:46 -0700

    Fix warning in cbor

--- third_party/cbor/src/lib.rs
+++ third_party/cbor/src/lib.rs
@@ -148,6 +148,7 @@
 #![deny(missing_docs)]
 #![cfg_attr(not(target_env = "sgx"), no_std)]
 #![cfg_attr(target_env = "sgx", feature(rustc_private))]
+#![allow(deprecated)]
 
 #[cfg(not(target_env = "sgx"))]
 #[macro_use]
