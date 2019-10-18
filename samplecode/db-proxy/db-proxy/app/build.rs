dc12c4d299a926ebcf56993d4448815923ced496
webb.shi <bradyjoestar@gmail.com>
2019-10-17 23:00:18 -0400

    add rust-poc and remove go-poc

--- samplecode/db-proxy/db-proxy/app/build.rs
+++ samplecode/db-proxy/db-proxy/app/build.rs
@@ -28,12 +28,9 @@
 
 use std::env;
 
-fn main () {
-
-    let sdk_dir = env::var("SGX_SDK")
-                    .unwrap_or_else(|_| "/opt/intel/sgxsdk".to_string());
-    let is_sim = env::var("SGX_MODE")
-                    .unwrap_or_else(|_| "HW".to_string());
+fn main() {
+    let sdk_dir = env::var("SGX_SDK").unwrap_or_else(|_| "/opt/intel/sgxsdk".to_string());
+    let is_sim = env::var("SGX_MODE").unwrap_or_else(|_| "HW".to_string());
 
     println!("cargo:rustc-link-search=native=../lib");
     println!("cargo:rustc-link-lib=static=Enclave_u");
@@ -42,6 +39,6 @@ fn main () {
     match is_sim.as_ref() {
         "SW" => println!("cargo:rustc-link-lib=dylib=sgx_urts_sim"),
         "HW" => println!("cargo:rustc-link-lib=dylib=sgx_urts"),
-        _    => println!("cargo:rustc-link-lib=dylib=sgx_urts"), // Treat undefined as HW
+        _ => println!("cargo:rustc-link-lib=dylib=sgx_urts"), // Treat undefined as HW
     }
 }
