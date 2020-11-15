31251991c6656e9418d6acbf076c77f0647812ea
Yu Ding <dingelish@gmail.com>
2020-11-15 14:50:32 -0800

    action: enable unit test

---
+++ samplecode/unit-test/enclave/build.rs
@@ -0,0 +1,14 @@
+use std::env;
+
+fn main () {
+    let is_sim = env::var("SGX_MODE")
+                    .unwrap_or_else(|_| "HW".to_string());
+
+    match is_sim.as_ref() {
+        "SW" => {
+        },
+        _    => { // HW by default
+            println!("cargo:rustc-cfg=feature=\"hw_test\"");
+        },
+    }
+}
