5f037b6458da44acd0569615385b24c1be5e6937
volcano <volcano_dr@163.com>
2021-10-14 19:41:20 +0800

    Fix switchless samplecode link error

--- samplecode/switchless/app/build.rs
+++ samplecode/switchless/app/build.rs
@@ -28,6 +28,7 @@ fn main () {
     println!("cargo:rustc-link-lib=static=Enclave_u");
 
     println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
+    println!("cargo:rustc-link-lib=static=sgx_uswitchless");
     match is_sim.as_ref() {
         "SW" => println!("cargo:rustc-link-lib=dylib=sgx_urts_sim"),
         "HW" => println!("cargo:rustc-link-lib=dylib=sgx_urts"),
