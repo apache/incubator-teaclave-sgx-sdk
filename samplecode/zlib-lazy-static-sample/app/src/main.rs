c6d7523b5cf7ee16b7f0c20f1dcfcf63d40f3070
Yu Ding <dingelish@gmail.com>
2022-03-23 13:59:18 -0700

    add zlib-lazy-static-sample

---
+++ samplecode/zlib-lazy-static-sample/app/src/main.rs
@@ -0,0 +1,58 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License..
+
+extern crate sgx_types;
+extern crate sgx_urts;
+use sgx_types::error::SgxStatus;
+use sgx_types::types::*;
+use sgx_urts::enclave::SgxEnclave;
+
+static ENCLAVE_FILE: &'static str = "enclave.signed.so";
+
+extern {
+    fn zlib_sample(eid: EnclaveId, retval: *mut SgxStatus) -> SgxStatus;
+}
+
+fn main() {
+
+    let enclave = match SgxEnclave::create(ENCLAVE_FILE, true) {
+        Ok(enclave) => {
+            println!("[+] Init Enclave Successful {}!", enclave.eid());
+            enclave
+        },
+        Err(err) => {
+            println!("[-] Init Enclave Failed {}!", err.as_str());
+            return;
+        },
+    };
+
+    let mut retval = SgxStatus::Success;
+
+    let result = unsafe {
+        zlib_sample(enclave.eid(),&mut retval)
+    };
+
+    match result {
+        SgxStatus::Success=> {},
+        _ => {
+            println!("[-] ECALL Enclave Failed {}!", result.as_str());
+            return;
+        }
+    }
+
+    println!("[+] zlib sample case ended!");
+}
---
+++ samplecode/zlib-lazy-static-sample/enclave/src/lib.rs
@@ -0,0 +1,52 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License..
+
+extern crate sgx_types;
+#[macro_use]
+extern crate lazy_static;
+
+use sgx_types::error::SgxStatus;
+use std::str::from_utf8;
+
+use inflate::inflate_bytes_zlib;
+use libflate::zlib::Encoder;
+use std::io::Write;
+
+lazy_static! {
+    static ref HELLOSTR : String = String::from("This is a global rust String init by lazy_static!");
+}
+
+#[no_mangle]
+pub extern "C"
+fn zlib_sample() -> SgxStatus {
+    println!("Source string is : {:?}", *HELLOSTR);
+    println!("Source data is : {:?}", HELLOSTR.as_bytes());
+
+    let mut encoder = Encoder::new(Vec::new()).unwrap();
+    encoder.write_all(HELLOSTR.as_bytes()).unwrap();
+    let encoded_data = encoder.finish().into_result().unwrap();
+
+    println!("After zlib compress : {:?}", encoded_data);
+
+    let decoded = inflate_bytes_zlib(&encoded_data[..]).unwrap();
+
+    let decoded_string = from_utf8(&decoded[..]);
+    println!("After zlib decompress: {:?}", decoded_string.unwrap());
+
+    SgxStatus::Success
+}
+
