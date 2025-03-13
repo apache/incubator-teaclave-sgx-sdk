b5901a30f23ac3e31b05f35609d1ac9b0651ecb2
xuewenjie <xuewenjie@xiaohongshu.com>
2025-03-13 07:31:08 +0000

    fix: add build.rs for enclave build

--- samplecode/new_helloworld/enclave/build.rs
+++ samplecode/new_helloworld/enclave/build.rs
@@ -1,17 +1,42 @@
+use std::process::Command;
+use std::env;
+
 fn main() {
-    // 获取当前工作目录
-    // let current_dir = std::env::current_dir().unwrap();
-    // 设置链接器脚本路径
-    // let version_script_path = current_dir.join("enclave.lds");
-    println!("cargo:rerun-if-env-changed=SGX_MODE");
     println!("cargo:rerun-if-changed=build.rs");
-    // println!("cargo:rerun-if-changed=enclave.lds");
-    // println!(
-    //     "cargo:rustc-flags=-Wl --version-script={}",
-    //     version_script_path.display()
-    // );
-    // println!(
-    //     "cargo:rustc-link-arg=-Wl,--version-script={}",
-    //     version_script_path.display()
-    // );
+
+    println!("cargo:rustc-link-arg=-Wl,--no-undefined");
+    println!("cargo:rustc-link-search=native=/opt/sgxsdk/lib64");
+    println!("cargo:rustc-link-arg=-nostdlib");
+    // println!("cargo:rustc-link-lib=sgx_tstdc");
+    println!("cargo:rustc-link-lib=static=sgx_trts");
+    println!("cargo:rustc-link-lib=static=sgx_tservice");
+    println!("cargo:rustc-link-arg=-nodefaultlibs");
+    println!("cargo:rustc-link-arg=-nostartfiles");
+    println!("cargo:rustc-link-arg=-Wl,--version-script=enclave/enclave.lds");
+    println!("cargo:rustc-link-arg=-Wl,-z,relro,-z,now,-z,noexecstack");
+    println!("cargo:rustc-link-arg=-Wl,-Bstatic");
+    println!("cargo:rustc-link-arg=-Wl,-Bsymbolic");
+    println!("cargo:rustc-link-arg=-Wl,--no-undefined");
+    println!("cargo:rustc-link-arg=-Wl,-pie");
+    println!("cargo:rustc-link-arg=-Wl,--export-dynamic");
+    println!("cargo:rustc-link-arg=-Wl,--gc-sections");
+
 }
+
+
+// fn main() {
+//     // 获取当前工作目录
+//     // let current_dir = std::env::current_dir().unwrap();
+//     // 设置链接器脚本路径
+//     // let version_script_path = current_dir.join("enclave.lds");
+    
+//     // println!("cargo:rerun-if-changed=enclave.lds");
+//     // println!(
+//     //     "cargo:rustc-flags=-Wl --version-script={}",
+//     //     version_script_path.display()
+//     // );
+//     // println!(
+//     //     "cargo:rustc-link-arg=-Wl,--version-script={}",
+//     //     version_script_path.display()
+//     // );
+// }
